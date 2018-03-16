d7e9a04125e36902addab8d888969cf9043126bb
repelentwizard <34221389+repelentwizard@users.noreply.github.com>
2018-03-16 18:21:21 -0500

    Add files via upload

---
+++ Chapter4Java/chapter4/fl6.java
@@ -0,0 +1,26 @@
+/*
+Tyler Smith
+Java Per. 4
+Assignment = fl6
+*/
+import java.io.*;
+import java.util.Scanner;
+import java.text.*;
+public class fl6
+{
+	public static void main(String[] args)
+	{
+	double num = 5;
+	double num1 = 0;
+	double num2;
+	Scanner s = new Scanner(System.in);
+        for (num2 = 0; num2 < num; num2++) {
+          System.out.print("Inserts a number: ");						                num1 += s.nextInt();
+          }
+	  System.out.println("The sum is " + num1);
+          System.out.println("average is " + (num1 / num2));			    
+	}}
+
+
+
+
